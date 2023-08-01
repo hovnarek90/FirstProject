472c15ee49e83986cbbf0c627268e2f7ed589a80
Hov20 <82816475+Hov20@users.noreply.github.com>
2023-08-01 12:47:42 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import { useState } from "react";
+// import { useState } from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PassRegistration from "./components/Layouts/Registration/PassengerReg/PassangerRegistr";
-import DriverRegistration from "./components/Layouts/Registration/DriverReg/DriverRegistr";
- 
-// import Header from "./components/Layouts/Header/Header.jsx";
+import DriverRegistration from "./components/Layouts/Registration/DriverReg/DriverRegistr"; 
+import Header from "./components/Layouts/Header/Header.jsx";
 // import Main from "./components/Layouts/Main/Main";
 
 function App() {
-  const [pass, setPass] = useState(false);
-  const [driver, setDriver] = useState(false);
+  // const [pass, setPass] = useState(false);
+  // const [driver, setDriver] = useState(false);
 
   return (
     <>
-    <button type="button" onClick={()=>{setDriver(!driver)}}>Driver</button>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Header" element={<Header />} />
+        <Route path="/PassangerRegistr" element={<PassRegistration />} />
+        <Route path="/DriverRegistr" element={<DriverRegistration />} />
+        {/* <Route path="Main" element={<Main />} /> */}
+      </Routes>
+    </BrowserRouter>
+    {/* <button type="button" onClick={()=>{setDriver(!driver)}}>Driver</button>
       {driver ? <DriverRegistration /> : null }
     <button type="button" onClick={()=>{setPass(!pass)}}>Passenger</button>
-      {pass ? <PassRegistration /> : null }
-
-      {/* <Header />
-      <Main /> */}
+      {pass ? <PassRegistration /> : null } */}
     </>
   );
 }
