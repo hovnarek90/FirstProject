7ce5403b0c034823aa96765c1ab3c7b317dc0a77
Hov20 <82816475+Hov20@users.noreply.github.com>
2023-08-01 17:37:09 +0400

    Update PassangerRegistr.jsx

--- src/components/Layouts/Registration/PassengerReg/PassangerRegistr.jsx
+++ src/components/Layouts/Registration/PassengerReg/PassangerRegistr.jsx
@@ -6,7 +6,6 @@ const PassRegistration = () => {
   const persones = [];
   const [state, setState] = useState({
     fullname: "",
-    number: "",
     password: "",
   });
 
@@ -19,11 +18,10 @@ const PassRegistration = () => {
     console.log(persones);
     setState({
       fullname: "",
-      number: "",
       password: "",
     });
   };
-  
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setState({ ...state, [name]: value });
@@ -32,21 +30,14 @@ const PassRegistration = () => {
     let errors = {};
 
     const nameRegex = /^[a-zA-Z\s]+$/;
-    const phoneRegex = /^[0-9]{10}$/;
     const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
     if (!state.fullname.match(nameRegex)) {
-      errors.fullName =
-        "Please enter a valid full name (letters and spaces only).";
-    }
-
-    if (!state.number.match(phoneRegex)) {
-      errors.phoneNumber = "Please enter a valid phone number (10 digits).";
+      errors.fullName = "Error Name";
     }
 
     if (!state.password.match(passwordRegex)) {
-      errors.password =
-        "Password must be at least 8 characters long and contain at least one letter and one number.";
+      errors.password = "Error Password";
     }
 
     setErrors(errors);
@@ -56,45 +47,35 @@ const PassRegistration = () => {
     <div className={classes.general}>
       <form className={classes.resepList} onSubmit={handleSubmit}>
         <div className={classes.context}>
-          <h1>Registration</h1>
+          <h1>Register</h1>
         </div>
         <div className={classes.info}>
           <div className={classes.inpname}>
             <label htmlFor="fullname">Full name</label>
-            <br />
             <input
               id="fullname"
               name="fullname"
               type="text"
+              placeholder="Enter your full name"
               value={state.fullname}
               onChange={handleChange}
             />
+            <br />
             {errors.fullname && (
               <span className="error">{errors.fullname}</span>
             )}
           </div>
-          <div className={classes.inpnumber}>
-            <label htmlFor="number">PhonNum</label>
-            <br />
-            <input
-              id="number"
-              name="number"
-              type="tel"
-              value={state.number}
-              onChange={handleChange}
-            />
-            {errors.number && <span className="error">{errors.number}</span>}
-          </div>
           <div className={classes.inppassword}>
             <label htmlFor="myPassword">Password</label>
-            <br />
             <input
               id="myPassword"
               name="password"
               type="password"
+              placeholder="Enter your password"
               value={state.password}
               onChange={handleChange}
             />
+            <br />
             {errors.password && (
               <span className="error">{errors.password}</span>
             )}
@@ -102,10 +83,11 @@ const PassRegistration = () => {
         </div>
         <div className={classes.setbtn}>
           <button type="submit" id="mybtn">
-            Continue
+            Login
           </button>
         </div>
       </form>
+      
     </div>
   );
 };
